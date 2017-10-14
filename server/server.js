135c60c585cae91447611339de101a560905eda5
michal <gelhrid@gmail.com>
2017-10-14 09:33:51 +0200

    GET/:id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+const {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -21,11 +22,6 @@ app.post('/todos', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
-});
-
-
 app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
     res.send({todos})
@@ -34,6 +30,25 @@ app.get('/todos', (req, res) => {
   ).catch((e) => {
     res.status(400).send(e);
   })
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send('chyba cie pojebalo zly ID');
+  }
+  Todo.findById(id).then((todo) => {
+    if(!todo){
+        return res.status(404).send();
+    }
+      res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Started on port 3000');
 });
 
 module.exports = {app};
