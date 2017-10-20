30c33cef372b00378bed5758725edf1fb6e77e77
Michał Kodź <gelhrid@gmail.com>
2017-10-20 12:55:32 +0200

    dodanie delete tokent

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -238,7 +238,7 @@ describe('GET /todos', () => {
             expect(user).toExist();
             expect(user.password).toNotBe(password);
             done();
-          });
+          }).catch((e) => { done(e);});
         });
       });
 
@@ -273,4 +273,59 @@ describe('GET /todos', () => {
         .end(done);
         });
 
+    });
+
+    describe('POST /users/login', () => {
+      it('should user and return auth token', (done) => {
+        request(app)
+        .post('/users/login/')
+        .send({
+          email: users[1].email,
+          password: users[1].password
+        })
+        .expect(200)
+        .expect((res) =>{
+          expect(res.header['x-auth']).toExist();
+        })
+        .end((error, res) => {
+          if(error){
+            return done(error);
+          }
+
+          User.findById(users[1]._id).then((user) => {
+            expect(user.tokens[0]).toInclude({
+              access: 'auth',
+              token: res.headers['x-auth']
+            });
+            done();
+          }).catch((e) => {
+            done(e);
+          });
+        });
+      });
+
+      it('should reject invalid login', (done) => {
+        request(app)
+        .post('/users/login/')
+        .send({
+          email: users[1].email,
+          password: 'zleHaslo'
+        })
+        .expect(400)
+        .expect((res) =>{
+          expect(res.header['x-auth']).toNotExist();
+        })
+        .end((error, res) => {
+          if(error){
+            return done(error);
+          }
+
+          User.findById(users[1]._id).then((user) => {
+            expect(user.tokens.length).toBe(0);
+            done();
+          }).catch((e) => {
+            done(e);
+          });
+        });
+      });
     });
